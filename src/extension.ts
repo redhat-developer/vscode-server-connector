c856ed2f223bdb4de4f88cc14f176deff818c8d6
Denis Golovin <dgolovin@gmail.com>
2018-06-15 14:05:10 -0700

    Fix location selection and server starting sync

--- src/extension.ts
+++ src/extension.ts
@@ -12,60 +12,68 @@ import * as server from './server';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let serversData;
+    let serverPromise = server.start().then(connectionInfo => {
+        return new Promise((resolve, reject) => {
+            const socket = net.connect(connectionInfo).on('connect', async () => {
+                const connection = rpc.createMessageConnection(
+                    new rpc.StreamMessageReader(socket),
+                    new rpc.StreamMessageWriter(socket));
 
-    return server.start().then(connectionInfo => {
-        const socket = net.connect(connectionInfo).on('connect', async () => {
-            const connection = rpc.createMessageConnection(
-                new rpc.StreamMessageReader(socket),
-                new rpc.StreamMessageWriter(socket));
+                connection.listen();
 
-            connection.listen();
+                connection.onNotification(ServerAddedNotification.type, handle => {
+                    serversData.insertServer(handle);
+                });
 
-            connection.onNotification(ServerAddedNotification.type, handle => {
-                serversData.insertServer(handle);
-            });
+                connection.onNotification(ServerRemovedNotification.type, handle => {
+                    serversData.removeServer(handle);
+                });
 
-            connection.onNotification(ServerRemovedNotification.type, handle => {
-                serversData.removeServer(handle);
-            });
+                connection.onNotification(ServerStateChangeNotification.type, event => {
+                    serversData.updateServer(event);
+                });
 
-            connection.onNotification(ServerStateChangeNotification.type, event => {
-                serversData.updateServer(event);
-            });
+                connection.onNotification(ServerProcessOutputAppendedNotification.type, event => {
+                    serversData.addServerOutput(event);
+                });
 
-            connection.onNotification(ServerProcessOutputAppendedNotification.type, event => {
-                serversData.addServerOutput(event);
-            });
+                serversData = new ServersViewTreeDataProvider(connection);
+                vscode.window.registerTreeDataProvider('servers', serversData);
+                vscode.commands.registerCommand('server.start', context => {
+                    connection.sendRequest(StartServerAsyncRequest.type, {
+                        params: {
+                            serverType: context.type.id,
+                            id: context.id,
+                            attributes: new Map<string, any>()
+                        },
+                        mode: 'run'}
+                    );
+                });
+                vscode.commands.registerCommand('server.stop', context => {
+                    connection.sendRequest(StopServerAsyncRequest.type, {id: context.id, force: true});
+                });
+                vscode.commands.registerCommand('server.remove', context => {
+                    connection.sendNotification(DeleteServerNotification.type, {id: context.id, type: context.type});
+                });
+                vscode.commands.registerCommand('server.output', context => {
+                    serversData.showOutput(context);
+                });
 
-            const serversData = new ServersViewTreeDataProvider(connection);
-            vscode.window.registerTreeDataProvider('servers', serversData);
-            vscode.commands.registerCommand('servers.addLocation', () => {
-                serversData.addLocation();
+                context.subscriptions.push(connection);
+                resolve();
             });
-            vscode.commands.registerCommand('server.start', context => {
-                connection.sendRequest(StartServerAsyncRequest.type, {
-                    params: {
-                        serverType: context.type.id,
-                        id: context.id,
-                        attributes: new Map<string, any>()
-                    },
-                    mode: 'run'}
-                );
-            });
-            vscode.commands.registerCommand('server.stop', context => {
-                connection.sendRequest(StopServerAsyncRequest.type, {id: context.id, force: true});
-            });
-            vscode.commands.registerCommand('server.remove', context => {
-                connection.sendNotification(DeleteServerNotification.type, {id: context.id, type: context.type});
-            });
-            vscode.commands.registerCommand('server.output', context => {
-                serversData.showOutput(context);
-            });
-
-            context.subscriptions.push(connection);
         });
     }).catch(err => {
         console.log(err);
+    });
+
+    vscode.commands.registerCommand('servers.addLocation', () => {
+        if(serversData) {
+            serversData.addLocation();
+        } else {
+            vscode.window.showInformationMessage('Stack Protocol Server is starting, please try again later!');
+        }
     });
 }
 
