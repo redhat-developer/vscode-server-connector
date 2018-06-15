c905d7e2c3710ed839649ccaea658d3e6312df04
Denis Golovin <dgolovin@gmail.com>
2018-06-15 13:17:25 -0700

    Implement server start upd and waiting for connection

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
-// import * as cp from 'child_process';
+import * as cp from 'child_process';
 import * as findJava from 'find-java-home';
 import * as path from 'path';
+import * as waitOn from 'wait-on';
 
 export interface ConnectionInfo {
     host: string;
     port: number;
 }
 
-export function start(): Promise<ConnectionInfo>{
+export function start(): Promise<ConnectionInfo> {
     return new Promise((resolve, reject) => {
         findJava((err, home) => {
-            if(err) {
+            if (err) {
                 reject(err);
             } else {
                 let serverLocation = path.resolve(__dirname, '..', 'server');
@@ -18,10 +19,14 @@ export function start(): Promise<ConnectionInfo>{
                 let java = path.join(home, 'bin', 'java.exe');
                 console.log('Servers java location ' + java);
                 console.log('Servers felix location ' + felix);
-                // let result = cp.spawn(java, ['-jar', felix],{cwd: serverLocation});
-                resolve({
-                    port: 27511,
-                    host: 'localhost'
+                cp.spawn(java, ['-jar', felix], { cwd: serverLocation });
+                waitOn({
+                    resources: ['tcp:localhost:27511']
+                }, () => {
+                    resolve({
+                        port: 27511,
+                        host: 'localhost'
+                    });
                 });
             }
         });
