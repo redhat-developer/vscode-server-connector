30b040e646aeecb37275e900d97087cc035ea1f2
sudhirverma <sudhirvvermaa@gmail.com>
2018-06-27 17:26:42 +0530

    remove tool.ts

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,23 @@ import * as cp from 'child_process';
 import * as findJava from 'find-java-home';
 import * as path from 'path';
 import * as waitOn from 'wait-on';
+import * as vscode from 'vscode';
 
 export interface ConnectionInfo {
     host: string;
     port: number;
 }
 
-export function start(): Promise<ConnectionInfo> {
+export function start(context): Promise<ConnectionInfo> {
     return new Promise((resolve, reject) => {
         findJava((err, home) => {
             if (err) {
                 reject(err);
             } else {
+                const sspserverstdout = vscode.window.createOutputChannel('SSP Server (stdout)');
+                const sspserverstderr = vscode.window.createOutputChannel('SSP Server (stderr)');
+                context.subscriptions.push(sspserverstdout);
+                context.subscriptions.push(sspserverstderr);
                 const serverLocation = path.resolve(__dirname, '..', 'server');
                 const felix = path.join(serverLocation, 'bin', 'felix.jar');
                 const java = path.join(home, 'bin', 'java');
@@ -27,9 +32,17 @@ export function start(): Promise<ConnectionInfo> {
                     });
                 });
                 sspserver.stdout.on('data', data => {
-                    return `${data}`;
+                    displayLog(sspserverstdout, data.toString());
+                });
+                sspserver.stderr.on('data', data => {
+                    displayLog(sspserverstderr, data.toString());
                 });
             }
         });
     });
+}
+
+function displayLog(outputPanel: vscode.OutputChannel, message: string) {
+    outputPanel.show();
+    outputPanel.appendLine(message);
 }
