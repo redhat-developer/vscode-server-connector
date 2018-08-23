bdb3fffba3f070798638e45ae62cea155e0c0f39
Denis Golovin <dgolovin@redhat.com>
2018-08-23 14:30:29 -0700

    Remove dependencies to vscode form Server class

--- src/server.ts
+++ src/server.ts
@@ -2,47 +2,33 @@ import * as cp from 'child_process';
 import * as findJava from 'find-java-home';
 import * as path from 'path';
 import * as waitOn from 'wait-on';
-import * as vscode from 'vscode';
 
-export interface ConnectionInfo {
+export interface ServerInfo {
     host: string;
     port: number;
+    process: cp.ChildProcess;
 }
 
-export function start(context: vscode.ExtensionContext): Promise<ConnectionInfo> {
+export function start(): Promise<ServerInfo> {
     return new Promise((resolve, reject) => {
         findJava((err, home) => {
             if (err) {
                 reject(err);
             } else {
-                const rspserverstdout = vscode.window.createOutputChannel('RSP Server (stdout)');
-                const rspserverstderr = vscode.window.createOutputChannel('RSP Server (stderr)');
-                context.subscriptions.push(rspserverstdout);
-                context.subscriptions.push(rspserverstderr);
                 const serverLocation = path.resolve(__dirname, '..', '..', 'server');
                 const felix = path.join(serverLocation, 'bin', 'felix.jar');
                 const java = path.join(home, 'bin', 'java');
-                const rspserver = cp.spawn(java, ['-jar', felix], { cwd: serverLocation });
+                const process = cp.spawn(java, ['-jar', felix], { cwd: serverLocation });
                 waitOn({
                     resources: ['tcp:localhost:27511']
                 }, () => {
                     resolve({
                         port: 27511,
-                        host: 'localhost'
+                        host: 'localhost',
+                        process
                     });
-                });
-                rspserver.stdout.on('data', data => {
-                    displayLog(rspserverstdout, data.toString());
-                });
-                rspserver.stderr.on('data', data => {
-                    displayLog(rspserverstderr, data.toString());
                 });
             }
         });
     });
-}
-
-function displayLog(outputPanel: vscode.OutputChannel, message: string) {
-    outputPanel.show();
-    outputPanel.appendLine(message);
 }
