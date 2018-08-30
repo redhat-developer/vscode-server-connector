212854105383a6c9609da00f13ad912731a0d2cd
sudhirverma <sudhirvvermaa@gmail.com>
2018-08-30 20:36:05 +0530

    unit test for updateServer function

--- test/serverExplorer.test.ts
+++ test/serverExplorer.test.ts
@@ -2,7 +2,9 @@ import * as chai from 'chai';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import { ServersViewTreeDataProvider } from '../src/serverExplorer';
-// import * as vscode from 'vscode';
+// import {
+//     OutputChannel
+// } from 'vscode';
 import { RSPClient, Protocol} from 'rsp-client';
 
 const expect = chai.expect;
@@ -12,11 +14,6 @@ suite('Server explorer', () => {
     let sandbox: sinon.SinonSandbox;
     let clientStub: sinon.SinonStubbedInstance<RSPClient>;
     let serverExplorer: ServersViewTreeDataProvider;
-    // let OutputChannelStub: sinon.SinonStubbedInstance<vscode.OutputChannel>
-
-    // const fakeConnection = {
-    //     set: () => {}
-    // };
 
     const serverType: Protocol.ServerType = {
         description: 'a type',
@@ -24,19 +21,34 @@ suite('Server explorer', () => {
         visibleName: 'the type'
     };
 
+    const fakeConnection = {
+        get: () => {}
+    };
+
     const serverHandle: Protocol.ServerHandle = {
         id: 'id',
         type: serverType
     };
 
-    suite('insertServer', () => {
-        test('should able to insert the server', async () => {
-            serverExplorer = new ServersViewTreeDataProvider(clientStub);
-            sandbox = sinon.createSandbox();
-            const refreshStub = sandbox.stub(serverExplorer, 'refresh').returns(null);
-            serverExplorer.insertServer(serverHandle);
-            expect(refreshStub).calledOnce;
-        });
+    // const stateChange: Protocol.ServerStateChange = {
+    //     server: serverHandle,
+    //     state: 4
+    // };
+
+    test('should able to insert the server', async () => {
+        serverExplorer = new ServersViewTreeDataProvider(clientStub);
+        sandbox = sinon.createSandbox();
+        const refreshStub = sandbox.stub(serverExplorer, 'refresh').returns(null);
+        serverExplorer.insertServer(serverHandle);
+        expect(refreshStub).calledOnce;
+    });
+
+    test('should able to insert the server', async () => {
+        serverExplorer = new ServersViewTreeDataProvider(clientStub);
+        sandbox = sinon.createSandbox();
+        const refreshStub = sandbox.stub(serverExplorer, 'refresh').returns(null);
+        serverExplorer.removeServer(serverHandle);
+        expect(refreshStub).calledOnce;
     });
 
 });
