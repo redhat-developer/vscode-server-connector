baeafa6b202b0ad99e134d257aa824648e5fdc3d
sudhirverma <sudhirvvermaa@gmail.com>
2018-09-03 11:20:53 +0530

    Added test for removeServer function

--- test/serverExplorer.test.ts
+++ test/serverExplorer.test.ts
@@ -2,9 +2,6 @@ import * as chai from 'chai';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import { ServersViewTreeDataProvider } from '../src/serverExplorer';
-// import {
-//     OutputChannel
-// } from 'vscode';
 import { RSPClient, Protocol} from 'rsp-client';
 
 const expect = chai.expect;
@@ -21,19 +18,10 @@ suite('Server explorer', () => {
         visibleName: 'the type'
     };
 
-    const fakeConnection = {
-        get: () => {}
-    };
-
     const serverHandle: Protocol.ServerHandle = {
         id: 'id',
         type: serverType
     };
-
-    // const stateChange: Protocol.ServerStateChange = {
-    //     server: serverHandle,
-    //     state: 4
-    // };
 
     test('should able to insert the server', async () => {
         serverExplorer = new ServersViewTreeDataProvider(clientStub);
@@ -43,12 +31,14 @@ suite('Server explorer', () => {
         expect(refreshStub).calledOnce;
     });
 
-    test('should able to insert the server', async () => {
+    test('should able to removeServer the server ', async () => {
         serverExplorer = new ServersViewTreeDataProvider(clientStub);
-        sandbox = sinon.createSandbox();
-        const refreshStub = sandbox.stub(serverExplorer, 'refresh').returns(null);
+        const disposeSpy = sinon.stub(serverExplorer.serverOutputChannels, 'get').returns({
+            clear: () => {},
+            dispose: () => {}
+        });
+        sandbox.stub(serverExplorer, 'refresh')
         serverExplorer.removeServer(serverHandle);
-        expect(refreshStub).calledOnce;
-    });
-
+        expect(disposeSpy).calledOnce;
+    })
 });
