a485169043bcb261d0533432afb9064eb8e21340
Andre Dietisheim <adietish@redhat.com>
2019-02-01 18:49:40 +0100

    [220] allow configure jdk, prompt to download jdk if not present
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -12,51 +12,62 @@ const rspserverstdout = vscode.window.createOutputChannel('RSP Server (stdout)')
 const rspserverstderr = vscode.window.createOutputChannel('RSP Server (stderr)');
 
 export async function activate(context: vscode.ExtensionContext): Promise<ExtensionAPI> {
-    let commandHandler: CommandHandler;
-
     const serverInfo = await server.start(onStdoutData, onStderrData);
+
+    if (!serverInfo || !serverInfo.port) {
+      return Promise.reject('Failed to start the rsp server');
+    }
     client = new RSPClient('localhost', serverInfo.port);
     await client.connect();
 
     serversData = new ServersViewTreeDataProvider(client);
     vscode.window.registerTreeDataProvider('servers', serversData);
-    commandHandler = new CommandHandler(serversData, client);
+    const commandHandler = new CommandHandler(serversData, client);
     await commandHandler.activate();
-    const subscriptions = [
-        vscode.commands.registerCommand('server.start', context => executeCommand(commandHandler.startServer, commandHandler, 'run', context)),
-        vscode.commands.registerCommand('server.debug', context => executeCommand(commandHandler.startServer, commandHandler, 'debug', context)),
-        vscode.commands.registerCommand('server.stop', context => executeCommand(commandHandler.stopServer, commandHandler, context)),
-        vscode.commands.registerCommand('server.remove', context => executeCommand(commandHandler.removeServer, commandHandler, context)),
-        vscode.commands.registerCommand('server.output', context => executeCommand(commandHandler.showServerOutput, commandHandler, context)),
-        vscode.commands.registerCommand('servers.addLocation', () => executeCommand(commandHandler.addLocation, commandHandler)),
-        vscode.commands.registerCommand('server.restart', context => executeCommand(commandHandler.restartServer, commandHandler, context)),
-
-        vscode.commands.registerCommand('server.addDeployment', context => executeCommand(commandHandler.addDeployment, commandHandler, context)),
-        vscode.commands.registerCommand('server.removeDeployment', context => executeCommand(commandHandler.removeDeployment, commandHandler, context)),
-        vscode.commands.registerCommand('server.publishFull', context => executeCommand(commandHandler.fullPublishServer, commandHandler, context)),
-
-        rspserverstdout,
-        rspserverstderr
-    ];
-    subscriptions.forEach(element => {
-        context.subscriptions.push(element);
-    }, this);
+    registerCommands(commandHandler, context);
     return { serverInfo };
+}
+
+function registerCommands(commandHandler: CommandHandler, context: vscode.ExtensionContext) {
+  const newLocal = [
+    vscode.commands.registerCommand('server.start', context => executeCommand(commandHandler.startServer, commandHandler, 'run', context)),
+    vscode.commands.registerCommand('server.debug', context => executeCommand(commandHandler.startServer, commandHandler, 'debug', context)),
+    vscode.commands.registerCommand('server.stop', context => executeCommand(commandHandler.stopServer, commandHandler, context)),
+    vscode.commands.registerCommand('server.remove', context => executeCommand(commandHandler.removeServer, commandHandler, context)),
+    vscode.commands.registerCommand('server.output', context => executeCommand(commandHandler.showServerOutput, commandHandler, context)),
+    vscode.commands.registerCommand('servers.addLocation', () => executeCommand(commandHandler.addLocation, commandHandler)),
+    vscode.commands.registerCommand('server.restart', context => executeCommand(commandHandler.restartServer, commandHandler, context)),
+    vscode.commands.registerCommand('server.addDeployment', context => executeCommand(commandHandler.addDeployment, commandHandler, context)),
+    vscode.commands.registerCommand('server.removeDeployment', context => executeCommand(commandHandler.removeDeployment, commandHandler, context)),
+    vscode.commands.registerCommand('server.publishFull', context => executeCommand(commandHandler.fullPublishServer, commandHandler, context)),
+    rspserverstdout,
+    rspserverstderr
+  ];
+  const subscriptions = newLocal;
+  subscriptions.forEach(element => {
+    context.subscriptions.push(element);
+  }, this);
 }
 
 export function deactivate() {
     if (client) {
         if (serversData) {
             for (const val of serversData.serverStatus.values()) {
-                const oneStat: Protocol.ServerState = val;
-                const stateNum = oneStat.state;
-                if (stateNum !== ServerState.UNKNOWN && stateNum !== ServerState.STOPPED && stateNum !== ServerState.STOPPING) {
-                    client.stopServerAsync( {id: oneStat.server.id, force: true });
-                }
+                stopServer(val);
             }
         }
         client.shutdownServer();
     }
+}
+
+function stopServer(val: Protocol.ServerState) {
+  const oneStat: Protocol.ServerState = val;
+  const stateNum = oneStat.state;
+  if (stateNum !== ServerState.UNKNOWN
+    && stateNum !== ServerState.STOPPED
+    && stateNum !== ServerState.STOPPING) {
+    client.stopServerAsync({ id: oneStat.server.id, force: true });
+  }
 }
 
 function onStdoutData(data: string) {
@@ -65,7 +76,7 @@ function onStdoutData(data: string) {
 
 function onStderrData(data: string) {
     displayLog(rspserverstderr, data.toString());
-};
+}
 
 function displayLog(outputPanel: vscode.OutputChannel, message: string, show: boolean = true) {
     if (show) outputPanel.show();
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,69 @@
+import * as requirements from './requirements';
+import * as vscode from 'vscode';
 import * as cp from 'child_process';
-import * as findJava from 'find-java-home';
 import * as path from 'path';
 import * as waitOn from 'wait-on';
 
 const portfinder = require('portfinder');
+let javaHome: string;
+let port: number;
 
 export interface ServerInfo {
     host: string;
     port: number;
 }
 
-export function start(stdoutCballback : (data: string) => void, stderrCallback: (data: string) => void ): Promise<ServerInfo> {
-    return new Promise((resolve, reject) => {
-        findJava((err, home) => {
-            if (err) {
-                reject(err);
-            } else {
-                const serverLocation = process.env.RSP_SERVER_LOCATION ? process.env.RSP_SERVER_LOCATION : path.resolve(__dirname, '..', '..', 'server');
-                const felix = path.join(serverLocation, 'bin', 'felix.jar');
-                const java = path.join(home, 'bin', 'java');
-                portfinder.basePort = 27511;
-                portfinder.getPortPromise()
-                .then(serverport => {
-                    // Debuggable version
-                    //const process = cp.spawn(java, [`-Xdebug`, `-Xrunjdwp:transport=dt_socket,server=y,address=8001,suspend=n`, `-Drsp.server.port=${serverport}`, '-jar', felix], { cwd: serverLocation });
-                    // Production version
-                    const process = cp.spawn(java, [`-Drsp.server.port=${serverport}`, '-jar', felix], { cwd: serverLocation });
-                    process.stdout.on('data', stdoutCballback);
-                    process.stderr.on('data', stderrCallback);
-                    waitOn({
-                        resources: [`tcp:localhost:${serverport}`]
-                    }, () => {
-                        resolve({
-                            port: serverport,
-                            host: 'localhost'
-                        });
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-            }
+export function start(stdoutCallback: (data: string) => void, stderrCallback: (data: string) => void ): Promise<ServerInfo> {
+  return requirements.resolveRequirements()
+    .catch(error => {
+      // show error
+      vscode.window.showErrorMessage(error.message, error.label)
+        .then(selection => {
+          if (error.label && error.label === selection && error.openUrl) {
+              vscode.commands.executeCommand('vscode.open', error.openUrl);
+          }
         });
+      // rethrow to disrupt the chain.
+      throw error;
+    })
+    .then(requirements => {
+      javaHome = requirements.java_home;
+      return portfinder.getPortPromise();
+    })
+    .then(serverPort => {
+      port = serverPort;
+      const serverLocation = getServerLocation(process);
+      startServer(serverLocation, serverPort, javaHome, stdoutCallback, stderrCallback);
+      return waitOn({ resources: [`tcp:localhost:${serverPort}`] });
+    })
+    .then(() => {
+      if (!port) {
+        return Promise.reject('Could not allocate a port for the rsp server to listen on.');
+      } else {
+        return Promise.resolve({
+            port: port,
+            host: 'localhost'
+          });
+        }
+    })
+    .catch(error => {
+      console.log(error);
+      return Promise.reject(error);
     });
+}
+
+function getServerLocation(process: any): string {
+  return  process.env.RSP_SERVER_LOCATION ?
+    process.env.RSP_SERVER_LOCATION : path.resolve(__dirname, '..', '..', 'server');
+}
+
+function startServer(location: string, port: number, javaHome: string, stdoutCallback, stderrCallback): void {
+  const felix = path.join(location, 'bin', 'felix.jar');
+  const java = path.join(javaHome, 'bin', 'java');
+  // Debuggable version
+  // const process = cp.spawn(java, [`-Xdebug`, `-Xrunjdwp:transport=dt_socket,server=y,address=8001,suspend=n`, `-Drsp.server.port=${serverport}`, '-jar', felix], { cwd: serverLocation });
+  // Production version
+  const process = cp.spawn(java, [`-Drsp.server.port=${port}`, '-jar', felix], { cwd: location });
+  process.stdout.on('data', stdoutCallback);
+  process.stderr.on('data', stderrCallback);
 }
