ef901c28c562c254c48adf985a7e14b5f92ec7ba
Luca Stocchi <lstocchi@redhat.com>
2019-06-06 15:02:03 +0200

    add editor stuff to allow user to edit server properties (#352)

--- src/editorutil.ts
+++ src/editorutil.ts
@@ -4,10 +4,16 @@
  *-----------------------------------------------------------------------------------------------*/
 
 'use strict';
+import * as fs from 'fs';
+import { ServerExplorer } from './serverExplorer';
 import * as vscode from 'vscode';
 
 export class EditorUtil {
-    constructor() {
+
+    private explorer: ServerExplorer;
+    
+    constructor(explorer: ServerExplorer) {
+        this.explorer = explorer;
     }
     public async showEditor(fileSuffix: string, content: string) {
         const newFile = vscode.Uri.parse('untitled:' + fileSuffix);
@@ -22,5 +28,35 @@ export class EditorUtil {
                 vscode.window.showInformationMessage('Error Displaying Editor Content');
             }
         });
+    }
+
+    public async _onDidSaveTextDocument(doc: vscode.TextDocument) {
+        if (!doc) {
+            return;
+        }
+        if (await EditorUtil.checkTmpServerPropsFile(doc)) {
+           const contentSaved = doc.getText();
+           this.explorer.editServer
+           //send to server
+        }
+    }
+
+    public async _onDidCloseTextDocument(doc: vscode.TextDocument) {
+        if (!doc) {
+            return;
+        }
+        if (await EditorUtil.checkTmpServerPropsFile(doc)) {
+            fs.unlink(doc.uri.fsPath, (error) => {
+                console.log(error);
+            });
+        }
+    }
+
+    private static async checkTmpServerPropsFile(doc: vscode.TextDocument): Promise<boolean> {
+        if (doc.fileName.indexOf('tmpServerConnectorProp') > -1) {
+            return true;
+        }
+
+        return false;
     }
 }
