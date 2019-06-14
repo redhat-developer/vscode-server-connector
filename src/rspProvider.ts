10b7a1f81fe65f9e77faee2cf5437c3027b4dab8
Luca Stocchi <lstocchi@redhat.com>
2019-06-14 18:08:28 +0200

    added rspprovider class (#370)

---
+++ src/rspProvider.ts
@@ -0,0 +1,25 @@
+export class RSPProvider {
+
+    constructor(private host: string, private port: string) {
+
+    }
+
+    public async startRSP() {
+
+    }
+
+    public async stopRSP() {
+
+    }
+
+    public getHost() {
+        return this.host;
+    }
+
+    public getPort() {
+        return this.port;
+    }
+
+
+
+}
