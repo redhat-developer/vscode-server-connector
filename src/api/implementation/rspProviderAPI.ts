acbca0ce813665ee40410e43c4ea57adde135141
Luca Stocchi <lstocchi@redhat.com>
2019-06-25 10:14:42 +0200

    changed type to register rsp provider to make external api thinner (#370)

--- src/api/implementation/rspProviderAPI.ts
+++ src/api/implementation/rspProviderAPI.ts
@@ -1,7 +1,7 @@
 import { API } from '../contract/api';
 import { available } from '../implementation/apiUtils';
 import { RSPProviderAPI } from '../contract/rspProviderAPI';
-import { RSPProperties, RSPState, ServerExplorer } from '../../serverExplorer';
+import { RSPProperties, RSPType, ServerExplorer } from '../../serverExplorer';
 import * as vscode from 'vscode';
 
 export function api(): API<RSPProviderAPI> {
@@ -12,10 +12,15 @@ export function impl(): RSPProviderAPI {
     return new RSPProviderAPIImpl();
 }
 
+interface RSPServer {
+    type: RSPType;
+    state: number;
+}
+
 class RSPProviderAPIImpl implements RSPProviderAPI {
     constructor() {}
 
-    public async registerRSPProvider(rsp: RSPState): Promise<void> {
+    public async registerRSPProvider(rsp: RSPServer): Promise<void> {
         let error: string;
         if (!rsp) {
             error = 'Unable to register RSP provider - RSP state is not valid.';
@@ -32,7 +37,7 @@ class RSPProviderAPIImpl implements RSPProviderAPI {
         const rspserverstdout = vscode.window.createOutputChannel(`${rsp.type.visibilename} (stdout)`);
         const rspserverstderr = vscode.window.createOutputChannel(`${rsp.type.visibilename} (stderr)`);
         const rspProperties: RSPProperties = {
-            state: rsp,
+            state: { ...rsp, serverStates: undefined },
             client: undefined,
             rspserverstderr: rspserverstderr,
             rspserverstdout: rspserverstdout
