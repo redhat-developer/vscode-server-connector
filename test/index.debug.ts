ded30f968de86e31208f08b02b60916c748275cb
Ondrej Dockal <odockal@redhat.com>
2019-11-22 07:10:35 +0100

    Move to vscode-test    - fixing adapters build    - updated package.json, package-lock    - removed vscode dependency
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- test/index.debug.ts
+++ test/index.debug.ts
@@ -3,22 +3,52 @@
  * All rights reserved. This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v20.html
- * 
+ *
  * Contributors: Red Hat, Inc.
  ******************************************************************************/
 
-import * as testRunner from 'vscode/lib/testrunner';
+import * as glob from 'glob';
+import * as Mocha from 'mocha';
+import * as path from 'path';
 
-process.on('unhandledRejection', (err) => {
+process.on('unhandledRejection', err => {
     console.log('Unhandled rejection:', err);
 });
 
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd',
-    useColors: true,
-    timeout: 50000,
-    slow: 50000
-});
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+        useColors: true,
+        timeout: 50000,
+        slow: 50000
+    });
+    mocha.useColors(true);
 
-module.exports = testRunner;
+    const testsRoot = path.resolve(__dirname);
+
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
+            }
+        });
+    });
+}
---
+++ test/test-runner.ts
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
+
+import * as path from 'path';
+
+import { runTests } from 'vscode-test';
+
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+        // The path to the extension test runner script
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, '../../out/test/');
+
+        // Download VS Code, unzip it and run the integration test
+        console.log(extensionDevelopmentPath, extensionTestsPath);
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+main();
