abc5ca37768b0b53502cd15e63fd3efbc3e8ce0d
Ondrej Dockal <odockal@redhat.com>
2020-03-25 11:56:49 +0100

    Adding extension assets UI tests    - initial ui tests using vscode-extension-tester    - checks necessary extensions    - added test trigger for travis and Jenkins    - update jenkinsfile, change to rhel8 due to ui tests
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

---
+++ src/ui-test/common/adaptersContants.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Ondrej Dockal <odockal@redhat.com>
+ */
+export class AdaptersConstants {
+
+    public static readonly RSP_UI_NAME = 'Remote Server Protocol UI';
+    public static readonly RSP_CONNECTOR_NAME = 'Server Connector';
+    public static readonly RSP_COMMAND = 'Servers:';
+    public static readonly RSP_SERVERS_LABEL = 'Servers';
+    public static readonly RSP_ATIVITY_BAR_TITLE = 'SERVER CONNECTOR: SERVERS';
+    public static readonly RSP_SERVER_ACTION_BUTTON = 'Create New Server...';
+}
