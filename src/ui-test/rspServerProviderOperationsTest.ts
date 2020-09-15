b7cc2397fdd683c8caf65bb3ee4f726147bf6956
Ondrej Dockal <odockal@redhat.com>
2020-09-15 22:03:10 +0200

    Code formating
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- src/ui-test/rspServerProviderOperationsTest.ts
+++ src/ui-test/rspServerProviderOperationsTest.ts
@@ -1,6 +1,6 @@
 import { WebDriver, VSBrowser, NotificationType, Workbench, InputBox } from "vscode-extension-tester";
 import { RSPServerProvider } from "./server/ui/rspServerProvider";
-import { serverHasState, notificationExistsWithObject, getNotifications } from "./common/util/serverUtils";
+import { serverHasState, notificationExistsWithObject } from "./common/util/serverUtils";
 import { expect } from 'chai'
 import { fail } from "assert";
 import * as os from "os";
@@ -9,7 +9,6 @@ import { ServersConstants } from "./common/serverConstants";
 import { ServerState } from "./common/enum/serverState";
 import { downloadableListIsAvailable } from "./common/util/downloadServerUtil";
 import { ServersTab } from "./server/ui/serversTab";
-
 
 const ERROR_CREATE_NEW_SERVER = 'Unable to create the server';
 const ERROR_NO_RSP_PROVIDER = 'there are no rsp providers to choose from';
@@ -40,56 +39,58 @@ export function rspServerProviderActionsTest() {
             serverProvider = await serversTab.getServerProvider(AdaptersConstants.RSP_SERVER_PROVIDER_NAME);
             const state = await serverProvider.getServerState();
             if (state === ServerState.Unknown || state === ServerState.Starting) {
-                await driver.wait(async () => await serverHasState(serverProvider, ServerState.Started), 15000 , 'Server was not started within 15 s on startup');
-            }
-            else if (state !== ServerState.Started) {
-                await serverProvider.start(20000);
+                await driver.wait(async () => await serverHasState(serverProvider, ServerState.Started),
+                10000 ,
+                'Server was not started within 10 s on startup');
+            } else if (state !== ServerState.Started) {
+                await serverProvider.start(10000);
             }
         });
 
+        it('Verify rsp server provider operation - stop', async function() {
+            this.timeout(8000);
+            await serverProvider.stop();
+        });
+
+
+        it('Verify rsp server provider operation - terminate', async function() {
+            this.timeout(8000);
+            await serverProvider.terminate();
+        });
+
         it('Verify rsp server provider operation - Create New Server', async function() {
-            this.timeout(20000);
+            this.timeout(10000);
             const quick = await serverProvider.getCreateNewServerBox();
             const options = await quick.getQuickPicks();
             expect(await Promise.all(options.map(async item => await item.getText()))).to.have.members([YES, USE_FROM_DISK]);
             await quick.selectQuickPick(YES);
-            await VSBrowser.instance.driver.wait( async () =>  await downloadableListIsAvailable(quick), 5000 );
+            await VSBrowser.instance.driver.wait( async () => await downloadableListIsAvailable(quick), 5000 );
             const input = await InputBox.create();
-            await input.setText('WildFly 20');
+            await input.setText('WildFly 19');
             const optionsText = await Promise.all((await input.getQuickPicks()).map(async item => await item.getText()));
             await input.clear();
             await input.setText('Red Hat EAP');
-            optionsText.push(...(await Promise.all((await input.getQuickPicks()).map(async item => await item.getText()))));
+            optionsText.push(...(await Promise.all((await input.getQuickPicks()).map(async item => (await item.getText())))));
             expect(optionsText).to.include.members(Object.keys(ServersConstants.TEST_SERVERS).map(key => ServersConstants.TEST_SERVERS[key]));
             await quick.cancel();
         });
 
-        it('Verify rsp server provider operation - stop', async function() {
-            this.timeout(20000);
-            await serverProvider.stop();
-        });
-
         it('Verify rsp server provider behavior - cannot create new server on stopped rsp provider', async function() {
-            this.timeout(20000);
+            this.timeout(10000);
             await serverProvider.stop();
             // normally we would be expecting input box to appear
             await serverProvider.createNewServerCommand();
             let notification;
-            try {
-                notification = await driver.wait(() => notificationExistsWithObject(ERROR_CREATE_NEW_SERVER), 3000 );
+            try { 
+                notification = await driver.wait(() => { return notificationExistsWithObject(ERROR_CREATE_NEW_SERVER);}, 3000 );
             } catch (error) {
                 console.log(error);
                 const nc = await new Workbench().openNotificationsCenter();
-                fail('Failed to obtain Create new server warning notification, available notifications are: '
-                + (await Promise.all((await nc.getNotifications(NotificationType.Any)).map(async item => await item.getText()))));
+                fail('Failed to obtain Create new server warning notification, available notifications are: ' 
+                + (await Promise.all((await nc.getNotifications(NotificationType.Any)).map(async (item) => await item.getText()))));
             }
             expect(await notification.getType()).equals(NotificationType.Warning);
             expect(await notification.getMessage()).to.include(ERROR_NO_RSP_PROVIDER);
-        });
-
-        it('Verify rsp server provider operation - terminate', async function() {
-            this.timeout(20000);
-            await serverProvider.terminate();
         });
 
         afterEach(async function() {
@@ -100,13 +101,6 @@ export function rspServerProviderActionsTest() {
             } catch (error) {
                 // no input box, not need to close it
             }
-            const errors = await getNotifications(NotificationType.Error);
-            if (errors && errors.length > 0) {
-                const report = errors.map(async error => {
-                    return `${await error.getSource()} ${await error.getMessage()} \r\n`;
-                });
-                console.log('Error appeared during creating local server adapter: ' + report);
-            }
             // clean up notifications
             const nc = await new Workbench().openNotificationsCenter();
             const notifications = await nc.getNotifications(NotificationType.Any);
@@ -114,25 +108,6 @@ export function rspServerProviderActionsTest() {
                 await nc.clearAllNotifications();
             }
             await nc.close();
-        });
-
-        after(async function() {
-            this.timeout(20000);
-            // Check error messages if any
-            const errors = await getNotifications(NotificationType.Error);
-            if (errors && errors.length > 0) {
-                const report = errors.map(async error => {
-                    return `${await error.getSource()} ${await error.getMessage()} \r\n`;
-                });
-                console.log('Error appeared during creating local server adapter: ' + report);
-            }
-            const tab = new ServersTab();
-            await tab.open();
-            const provider = await tab.getServerProvider(AdaptersConstants.RSP_SERVER_PROVIDER_NAME);
-            const state = await provider.getServerState();
-            if (![ServerState.Stopped, ServerState.Stopping].includes(state)) {
-                provider.stop(10000);
-            }
         });
     });
 }
