41121ac6750d386c769e43dc5d1f3b12d6b50c53
Victor Rodrigues <victortavi@outlook.com>
2022-06-12 20:53:54 -0300

    The deprecated "java.home" configuration tag has been replaced by "java.jdt.ls.java.home"
    This configuration tag is already deprecated in current versions of vscode with the proper java extensions installed, the following annoying message is displayed "This setting is deprecated, please use 'java.jdt.ls.java.home' instead."

--- src/requirements.ts
+++ src/requirements.ts
@@ -43,7 +43,7 @@ function checkJavaRuntime(): Promise<string> {
         let source: string;
         let javaHome: string | undefined = readJavaConfig();
         if (javaHome) {
-            source = 'The java.home variable defined in VS Code settings';
+            source = 'The java.jdt.ls.java.home variable defined in VS Code settings';
         } else {
             javaHome = process.env.JDK_HOME;
             if (javaHome) {
@@ -76,7 +76,7 @@ function checkJavaRuntime(): Promise<string> {
 
 function readJavaConfig(): string {
     const config = workspace.getConfiguration();
-    return config.get<string>('java.home', '');
+    return config.get<string>('java.jdt.ls.java.home', '');
 }
 
 function checkJavaVersion(javaHome: string): Promise<number> {
